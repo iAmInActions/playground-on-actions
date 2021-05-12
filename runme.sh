bf6a6bf3b5b439caa51efcf03bcac6c8e94cf5be
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-12 20:27:44 +0200

    Create runme.sh

---
+++ runme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# DO NOT EDIT!!!
+# This script is for setting up ubuntu so it has the same features as playground. And for adding the web server.
+
+# Dependency set up
+sudo apt update
+sudo apt install nodejs npm wget nano git
+
+# Forward ports
+npm install -g localtunnel
+lt --port 80 --subdomain playground-on-actions-80 &
+lt --port 8080 --subdomain playground-on-actions-8080 &
+lt --port 9443 --subdomain playground-on-actions-cli &
+
+# Set up command line access
+cd webcli
+sudo ./mako mako -l::www
