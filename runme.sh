b46c1b04ca427450b193732ad669f11441187f80
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-13 13:06:19 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -3,8 +3,9 @@
 # This script is for setting up ubuntu so it has the same features as playground. And for adding the http web server.
 
 # Dependency set up
+sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install nodejs npm wget nano git
+sudo apt install nodejs npm wget nano git lua5.3
 
 # Forward ports
 sudo npm install -g localtunnel
